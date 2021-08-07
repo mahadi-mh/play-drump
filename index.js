029c22913fbe50b1bc40de41b5daa88e3cb9e305
Mahadi Hasan <mahadi250721@gmail.com>
2021-08-07 22:00:40 +0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-window.addEventListener("keydown", function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    audio.play();
-    audio.currentTime = 0;
 
-})
+
+let audio;
+function getId(buttonId) {
+    let id = buttonId;
+
+    switch (id) {
+        case 'a':
+            audio = new Audio("../music/avengers-infinity-war-ringtone.mp3");
+            break;
+        
+        case 'b':
+            audio = new Audio("../music/wonder-woman-ringtone.mp3");
+            break;
+        
+        case 'c':
+            audio = new Audio("../music/narcos-ringtone.mp3");
+            break;
+        
+        case 'd':
+            audio = new Audio("../music/peaky-blinders-ringtone.mp3");
+            break;
+        
+        case 'e':
+            audio = new Audio("../music/stranger-things-theme-song.mp3");
+            break;
+        
+        case 'f':
+            audio = new Audio("../music/thor-ragnarok-ringtone.mp3");
+            break;
+        
+    }
+    audio.play();
+}
